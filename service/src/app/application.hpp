c25d5c82514b247be2e9c3f85c6530b174f1c8f5
Danyil Serafin <serafin.danyil14@gmail.com>
2026-06-13 12:50:33 +0100

    feat: add configuration handling in Application class and update main function

--- service/src/app/application.hpp
+++ service/src/app/application.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
+#include "config/config.hpp"
+
 #include <atomic>
 
 class Application {
 public:
+    explicit Application(const Config& config);
     int run();
     void stop();
 
 private:
+    Config config_;
     std::atomic<bool> running_{true};
 };
