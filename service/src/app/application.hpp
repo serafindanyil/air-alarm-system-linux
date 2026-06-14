79a9b3e2cdeb7379d5e90c29ad9166dd17acc7f6
Danyil Serafin <serafin.danyil14@gmail.com>
2026-06-14 08:03:16 +0100

    feat: update Application constructor to include RegionList parameter

--- service/src/app/application.hpp
+++ service/src/app/application.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "config/config.hpp"
+#include "region_list/region_list.hpp"
 
 #include <atomic>
 
 class Application {
 public:
-    explicit Application(const Config& config);
+    explicit Application(const Config& config, const RegionList& region_list);
     int run();
     void stop();
 
 private:
     Config config_;
+    RegionList region_list_;
     std::atomic<bool> running_{true};
 };
