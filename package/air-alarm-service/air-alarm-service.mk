f10bd9ff0f7417fca3eeb5bdc57a0d4c4a2e4c3a
Danyil Serafin <serafin.danyil14@gmail.com>
2026-06-17 16:09:34 +0100

    feat: add air-alarm-list script and update region list path in main

--- package/air-alarm-service/air-alarm-service.mk
+++ package/air-alarm-service/air-alarm-service.mk
@@ -18,9 +18,17 @@ define AIR_ALARM_SERVICE_INSTALL_TARGET_CMDS
 		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/package/air-alarm-service/air-alarm-config \
 		$(TARGET_DIR)/usr/bin/air-alarm-config
 
+	$(INSTALL) -D -m 0755 \
+        $(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/package/air-alarm-service/air-alarm-list \
+        $(TARGET_DIR)/usr/bin/air-alarm-list
+
 	$(INSTALL) -D -m 0644 \
 		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/service/config/air_alarm_config.json \
 		$(TARGET_DIR)/etc/air-alarm-service/config.json
+
+	$(INSTALL) -D -m 0644 \
+		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/service/config/regions_adjacency_list.json \
+		$(TARGET_DIR)/etc/air-alarm-service/regions_adjacency_list.json
 
 	$(INSTALL) -D -m 0644 \
 		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/package/air-alarm-service/air-alarm-service.service \
