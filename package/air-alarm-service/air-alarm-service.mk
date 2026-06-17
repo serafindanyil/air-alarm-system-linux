ecf50e05c5b3deb11b3f9dbfc883059a3662452b
Danyil Serafin <serafin.danyil14@gmail.com>
2026-06-17 14:40:32 +0300

    feat: add air alarm service buildroot package

---
+++ package/air-alarm-service/air-alarm-service.mk
@@ -0,0 +1,35 @@
+AIR_ALARM_SERVICE_VERSION = 1.0
+AIR_ALARM_SERVICE_SITE = $(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/service
+AIR_ALARM_SERVICE_SITE_METHOD = local
+
+AIR_ALARM_SERVICE_DEPENDENCIES = \
+	nlohmann-json \
+	cpr \
+	libgpiod \
+	sdbus-cpp
+
+AIR_ALARM_SERVICE_INSTALL_STAGING = NO
+
+define AIR_ALARM_SERVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/air-alarm-service \
+		$(TARGET_DIR)/usr/bin/air-alarm-service
+
+	$(INSTALL) -D -m 0755 \
+		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/package/air-alarm-service/air-alarm-config \
+		$(TARGET_DIR)/usr/bin/air-alarm-config
+
+	$(INSTALL) -D -m 0644 \
+		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/service/config/air_alarm_config.json \
+		$(TARGET_DIR)/etc/air-alarm-service/config.json
+
+	$(INSTALL) -D -m 0644 \
+		$(BR2_EXTERNAL_AIR_ALARM_SYSTEM_PATH)/package/air-alarm-service/air-alarm-service.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/air-alarm-service.service
+
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+	ln -sf /usr/lib/systemd/system/air-alarm-service.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/air-alarm-service.service
+endef
+
+$(eval $(cmake-package))
